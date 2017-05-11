ca9f82132acd388ceed70998a4c91efc43ce0ff0
owenxbw <bx4y4@g.ucla.edu>
2017-05-10 23:36:39 -0700

    updated actor.sql

--- actors.sql
+++ actors.sql
@@ -2,7 +2,9 @@
 
 CREATE TABLE Actors (Name varchar(40), Movie varchar(80), Year int, Role varchar(40));
 
-load data local infile '~/data/actors.csv' into table Actors fields terminated by ',' optionally enclosed by '"';
+load data local infile '~/data/actor1.csv' into table Actors fields terminated by ',' optionally enclosed by '"';
+load data local infile '~/data/actor2.csv' into table Actors fields terminated by ',' optionally enclosed by '"';
+load data local infile '~/data/actor3.csv' into table Actors fields terminated by ',' optionally enclosed by '"';
 
 select Name from Actors where Movie='Die Another Day';
 
