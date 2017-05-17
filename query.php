5315074f95c1a886fb4bbf3555da84e768fea8b5
owenxbw <bx4y4@g.ucla.edu>
2017-05-16 22:14:19 -0700

    fixed POST stay on the same page

--- query.php
+++ query.php
@@ -1,30 +1,57 @@
-
 <html>
-			if(mysql_select_db($database, $db_connection)) { 
-<head><title>CS143 Project 1B Demo</title></head>
-<body>
-<p>
-Due to security concerns, this demo does not follow the spec exactly.
-It is just to give you a rough idea of how your code should work.</p>
+<head>
+<?php $title = "Hello World" ?>
+<title><?php print "$title"; ?></title>
+</head>
 
-<p>Please do not run a complex query here. You may kill the server. </p>
-Type an SQL query in the following box: <p>
-Example: <tt>SELECT * FROM Actor WHERE id=10;</tt><br />
-<p>
-<form action="." method="GET">
-<textarea name="query" cols="60" rows="8">SELECT * FROM Actor WHERE id=10</textarea><br />
-<input type="submit" value="Submit" />
-</form>
-</p>
-<p><small>Note: tables and fields are case sensitive. All tables in Project 1B are availale.</small>
-</p>
+<body bgcolor=white>
+<h1><?php print "$title"; ?></h1>
 
-<h3>Results from MySQL:</h3>
+echo "Type an SQL query in the following box:"<br>
+echo "Example: SELECT * FROM Actor WHERE id=10;"
 
-<table border=1 cellspacing=1 cellpadding=2>
-<tr align=center><td><b>id</b></td><td><b>last</b></td><td><b>first</b></td><td><b>sex</b></td><td><b>dob</b></td><td><b>dod</b></td></tr>
-<tr align=center><td>10</td><td>Aaltonen</td><td>Minna</td><td>Female</td><td>1966-09-17</td><td>N/A</td></tr>
-</table>
+<?php
+if(isset($_POST['SubmitButton'])){
+	$input=$_POST['name'];
+	echo "Success, You entered: ".$input;
+}
+
+?>
+<!-- <form action="." method="post">
+<textarea name="query" cols="60" rows="8"></textarea><br />
+<input type="submit">welcome.php
+</form> -->
+
+
+<form action="" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit" name="SubmitButton"/>
+
+<!-- <?php
+// $param = $_GET["param"];  //??
+// if($param) {
+//     print "Thanks for the lovely param='$param' binding.";
+// }
+
+
+
+
+
+//to MYSQL
+// $db_connection = mysql_connect("localhost", "cs143", "");
+// mysql_select_db("CS143", $db_connection);
+// $query = "select * from Student";	//inputs from user?
+// $rs = mysql_query($query, $db_connection);
+// while($row = mysql_fetch_row($rs)) {	//retrieving results  
+//     $sid = $row[0];
+//     $name = $row[1];
+//     $email = $row[2];
+//     print "$sid, $name, $email<br />";
+// }
+
+// mysql_close($db_connection);
+?> -->
 
 </body>
 </html>
