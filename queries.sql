3b158617fa16513d233f5264eebbe6716fd4c10b
owenxbw <bx4y4@g.ucla.edu>
2017-05-19 23:41:08 -0700

    queries

--- queries.sql
+++ queries.sql
@@ -5,12 +5,12 @@ SELECT CONCAT(first, " ", last) FROM Actor WHERE id IN
 	(SELECT aid FROM MovieActor WHERE mid IN 
 		(SELECT id FROM Movie WHERE title = 'Die Another Day')); 
 
---Give me the count of all the actors who acted in multiple movies.
+--Show the count of all the actors who acted in multiple movies.
 SELECT COUNT(*) FROM (
 	SELECT aid FROM MovieActor GROUP BY aid HAVING COUNT(aid) > 1
 ) AS A;
 
---Give me the title of movies that sell more than 1,000,000 tickets.
+--Show the title of movies that sell more than 1,000,000 tickets.
 SELECT title FROM Movie WHERE id IN
 	(SELECT mid FROM Sales WHERE ticketsSold > 1000000);
 
@@ -19,6 +19,6 @@ SELECT CONCAT(first, " ", last) FROM Actor WHERE id IN
 	(SELECT aid FROM MovieActor WHERE mid IN 
 		(SELECT mid FROM MovieRating WHERE rot > 75));
 
--- Retrieves number of movies in the "Adult" genre.
+-- Retrieve number of movies in the "Adult" genre.
 SELECT COUNT(*) FROM Movie WHERE id IN 
 	(SELECT mid FROM MovieGenre WHERE genre = "Adult");
